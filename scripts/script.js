84959aa963a8352b05a50deef3f36213bce9dc04
Alan <alan.edward.martin@gmail.com>
2020-05-26 11:44:14 -0400

    removed text-allign center and justify for more natural reading experience, changed google firebase logo to google logo.

--- scripts/script.js
+++ scripts/script.js
@@ -5,24 +5,20 @@ app.mobileMenu = $('.mobNav');
 
 // init
 app.init = () => {
-  console.log('hi')
   $('.hamburger').on('click', app.toggleMenu)
   $('.mobItem').on('click', app.closeMenu)
 }
 
-// toggle mobile menu
+// toggle mobile menu=
 app.toggleMenu = () => {
   $('.hamburger').toggleClass('hamburger--collapse is-active');
-  // $('.hamburger').toggleClass('hidden');
   $(app.mobileMenu).slideToggle()
 };
 
 app.closeMenu = () => {
-  console.log('hi');
   $('.hamburger').removeClass('hamburger--collapse is-active')
   $(app.mobileMenu).hide()
 };
-
 
 // doc ready
 $(function () {
